9f845c238803dcfbb819442c9c528f212b9a321c
ruben <rubenpeib@gmail.com>
2022-10-07 17:31:57 +0200

    Cambio a asincrono

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import {RECAPTCHA_V3_SITE_KEY, RecaptchaV3Module} from 'ng-recaptcha';
 
 import { AppComponent } from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import { SincronoComponent } from './components/sincrono/sincrono.component';
+import { AsincronoComponent } from './components/asincrono/asincrono.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SincronoComponent,
+    AsincronoComponent
   ],
   imports: [
     BrowserModule,
-    RecaptchaV3Module
+    RecaptchaV3Module,
+    HttpClientModule
+
 
   ],
   providers: [{
--- src/app/service/recaptcha.service.ts
+++ src/app/service/recaptcha.service.ts
@@ -1,5 +1,11 @@
 import {Inject, Injectable, PLATFORM_ID} from '@angular/core';
 import {isPlatformBrowser} from '@angular/common';
+
+import { Observable, throwError} from 'rxjs';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {catchError, map} from "rxjs/operators";
+
+
 @Injectable({
   providedIn: 'root'
 })
@@ -7,12 +13,17 @@ export class RecaptchaService {
 
   public isBrowser  = false;
 
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+  constructor(@Inject(PLATFORM_ID) private platformId: Object,     private http: HttpClient) {
     if (isPlatformBrowser(platformId)) {
       this.isBrowser = true;
     }
   }
 
+  /*
+  Modo de comunicación con el servidor síncrono
+  parametro token: string
+  return string
+   */
   getToken(token: string): string {
     if  (this.isBrowser == true){
       const xhr = new XMLHttpRequest();
@@ -22,6 +33,29 @@ export class RecaptchaService {
     } else {
       return 'false'
     }
+  }
+
+
+  /*
+  Modo de comunicación con el servidor asíncrono
+  parametro token: string
+  return Observable<any>
+   */
+  getTokenClientModule(token: string): Observable<any> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+      })
+    };
+      return this.http.post<any>( 'http://0.0.0.0:5000/api/v1/verificar/' + token +'/', httpOptions)
+        .pipe(
+          map((response) => response),
+          catchError((err) => {
+            console.log('error caught in service')
+            console.error(err);
+            return throwError(err);
+          })
+        );
   }
 
 }
